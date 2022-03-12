09986557e4e29c49b7e3b3dc5514317fc071d459
Vijendra <docs.vijendra@gmail.com>
2022-03-12 21:25:22 +0530

    working on

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
-const port =  process.env.PORT|| 7000
-const path = require('path')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT
 
-//setting up api url
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3000;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+// const port =  process.env.PORT|| 7000
+// const path = require('path')
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults()
+
+// //setting up api url
+// server.use(middlewares)
+// server.use(jsonServer.bodyParser)
 
 
-server.use(router)
-server.listen(port, () => console.log(`JSON Server is running on port ${port}`))
+// server.use(router)
+// server.listen(port, () => console.log(`JSON Server is running on port ${port}`))
